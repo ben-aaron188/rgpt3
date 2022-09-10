5bcd9116eb28c70dd5ecba750aa7e62083645266
ben-aaron188 <ben-aaron188@users.noreply.github.com>
2022-09-10 21:33:50 +0200

    fixes in test_request doc

--- R/test_request.R
+++ R/test_request.R
@@ -1,18 +1,18 @@
 #' Make a test request to the GPT-3 API
 #'
 #' @description
-#' `gpt3.test_request()` sends a basic [completion request](https://beta.openai.com/docs/api-reference/completions) to the Open AI GPT-3 API.
+#' `gpt3_test_request()` sends a basic [completion request](https://beta.openai.com/docs/api-reference/completions) to the Open AI GPT-3 API.
 #' @param verbose (boolean) if TRUE prints the actual prompt and GPT-3 completion of the test request (default: FALSE).
 #' @return A message of success or failure of the connection.
 #' @examples
-#' gpt3.test_request()
+#' gpt3_test_request()
 #' @export
-gpt3.test_request = function(verbose=F){
+gpt3_test_request = function(verbose=F){
 
   check_apikey_form()
 
   test_prompt = 'Write a story about R Studio:'
-  test_outout = gpt3.make_request(prompt_ = test_prompt
+  test_outout = gpt3_simple_request(prompt_ = test_prompt
                                   , max_tokens = 100)
   print(paste0('.. test successful ..'))
 
